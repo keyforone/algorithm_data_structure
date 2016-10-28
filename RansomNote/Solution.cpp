0d66226f6d5c5d061507b6a747dbd2d08660b392
Stan Zhao <stan.zhao@ericsson.com>
2016-10-28 17:17:11 +0800

    DEV: Problem Ransom Note
    Change-Id: I30905ef45400016b3245fadb216f7270b7b2f03b

---
+++ RansomNote/Solution.cpp
@@ -0,0 +1,18 @@
+bool canConstruct(string ransomNote, string magazine)
+{
+    int count[26] = {0};
+
+    for(int i = 0; i < magazine.length(); i++)
+    {
+	count[magazine[i]-'a']++;
+    }
+
+    for(int i = 0; i < ransomNote.length(); i++)
+    {
+	if(count[ransomNote[i]-'a'] == 0) return false;
+
+	count[ransomNote[i]-'a']--;
+    }
+
+    return true;
+}
